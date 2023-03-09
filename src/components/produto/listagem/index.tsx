786efd5eba36af932d883dda8b353863dcb3a96e
gustavo <guguiantunes@hotmail.com>
2023-03-09 09:19:36 -0400

    consulta com swr ao back-end realizada e retornando os produtos na tela

--- src/components/produto/listagem/index.tsx
+++ src/components/produto/listagem/index.tsx
@@ -2,23 +2,25 @@ import { Produto } from "app/models/produtos";
 import { Layout } from "components";
 import Link from "next/link";
 import { TabelaProdutos } from "./tabela";
+import useSWR from "swr";
+import { AxiosResponse } from "axios";
+import { httpClient } from "app/http";
 
 export const ListagemProdutos: React.FC = () => {
-  const produtos: Produto[] = [
-    {
-      id: "1",
-      sku: "HER3ASD",
-      nome: "Impressora",
-      preco: 23.58,
-    },
-  ];
+  const { data: result } = useSWR<AxiosResponse<Produto[]>>(
+    "api/produtos",
+    (url) => httpClient.get(url)
+  );
 
+  if (!result) {
+    return <div>Carregando...</div>;
+  }
   return (
     <Layout titulo="Produtos">
       <Link href={"/cadastros/produtos"}>
         <button className="button is-warning">Novo</button>
       </Link>
-      <TabelaProdutos produtos={produtos} />
+      <TabelaProdutos produtos={result.data} />
     </Layout>
   );
 };
